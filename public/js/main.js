908271e295803602de9f070fbacc465981976d34
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 17:12:08 -0500

    added console debug

--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
   // if we have a map, intialize it
   if (Map !== undefined) {
     var map = new Map(this);
+    console.log('trying to make a new map');
   }
 
 });
