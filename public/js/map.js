c64c1cc4468104496203a49ddc4ff4f228b67bb9
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 17:48:42 -0500

    changed to use google's sample code

--- public/js/map.js
+++ public/js/map.js
@@ -1,31 +1,11 @@
-/*
-	hints:
-	-implement the line with a google.maps.Polyline
-	-let google redraw the polyline (see .getPath()) when a new
-	point is added - don't delete the whole thing and add it again 
-	-use google.maps.Marker for markers
-	-use .setMap(null) do remove/delete either of these from your map
-	you're making work for yourself if you do it any other way
-*/
-
-
-
-// Our map object.
-var Map = function Map() {
-
-
-	// feel free to edit map options... we want to probably center by default on user location.
-	var mapOptions = {
-		disableDefaultUI: true,
-		zoom: 5,
-		center: new google.maps.LatLng(39.50, -98.35),
-		mapTypeId: google.maps.MapTypeId.ROADMAP
-	}
-
-
-	// init takes object of id 'map_canvas' and creates a map...
-	// TODO: use css to define map bounds.
-	this.init = function() { console.log("-- init the map"); this.map = new google.maps.Map($('#map_canvas')[0], mapOptions); }
-
-	this.init();
+var map;
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+  map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
 }
+
+google.maps.event.addDomListener(window, 'load', initialize);
