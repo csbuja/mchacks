e3775ca06d67073da4f1f3b037e52fdc58326100
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 17:25:25 -0500

    removed map init code

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
 
-  // if we have a map, intialize it
-  if (Map !== undefined) {
-    console.log('trying to make a google map');
-    //var map = new Map(this);
-  }
+  // add code here
 
 });
