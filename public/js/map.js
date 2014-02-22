dcd544c703db2230de83c9bd7454fe4d04ab5853
Jackson <jessupjn@umich.edu>
2014-02-22 17:54:12 -0500

    map options

--- public/js/map.js
+++ public/js/map.js
@@ -1,8 +1,10 @@
 var map;
 function initialize() {
   var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(45.5833, -73.7500)
+  	disableDefaultUI: true,
+    zoom: 5,
+    center: new google.maps.LatLng(45.5833, -73.7500),
+    mapTypeId: google.maps.MapTypeId.ROADMAP
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
