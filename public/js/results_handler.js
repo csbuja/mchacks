ec6ab39157af9f0601c05696272149c9e97827e4
Jackson <jessupjn@umich.edu>
2014-02-22 18:17:53 -0500

    working on results_handler

--- public/js/results_handler.js
+++ public/js/results_handler.js
@@ -4,9 +4,15 @@ $(document).ready(function () {
 	// FROM should automatically be set to current location (or a location chosen by user)
 	// DESTINATION triggers a search and loads results into the results_pg.
 	// on search, fill the results_pg with results.
-	$('.destination').change( function(){
-		$('.results_pg').empty();
+	$('#destination').change( function(){
 
+		console.log('perform search');
+		
+		$('.result').remove();
+
+		performSearch(function(){
+			//$('.results_pg #'+$('.results_pg').children().length).append( $('.result') );
+		});
 
 	});
 
