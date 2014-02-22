53182d9aadf7443d1f2fcd26b09ee9076e7b0154
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 17:24:24 -0500

    commented map declaration line for debugging

--- public/js/main.js
+++ public/js/main.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
   // if we have a map, intialize it
   if (Map !== undefined) {
     console.log('trying to make a google map');
-    var map = new Map(this);
+    //var map = new Map(this);
   }
 
 });
