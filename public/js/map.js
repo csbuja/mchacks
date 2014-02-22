e34fec615bd49e00340e533537cf0716292ab6c0
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 17:55:52 -0500

    removed roadmap definition, updated zoom

--- public/js/map.js
+++ public/js/map.js
@@ -1,10 +1,9 @@
 var map;
 function initialize() {
   var mapOptions = {
-  	disableDefaultUI: true,
-    zoom: 5,
-    center: new google.maps.LatLng(45.5833, -73.7500),
-    mapTypeId: google.maps.MapTypeId.ROADMAP
+    disableDefaultUI: true,
+    zoom: 8,
+    center: new google.maps.LatLng(45.5833, -73.7500)
   };
   map = new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
