9b8618eed01db82819c77096b9c74e36d59b96b9
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 22:27:23 -0500

    Update APIHandler.js

--- models/APIHandler.js
+++ models/APIHandler.js
@@ -1,39 +1,44 @@
-//var libxmljs = require('libxmljs');
-//var http = require('http');
-var xml2js = require('xml2js');
-var fs = require('fs');
+var http = require('http'),
+  fs = require('fs'),
+  xml2js = require('xml2js');
 
-/* return data to caller
- * param: (string) commandType
- *  valid values: routeList, routeConfig, predictions,
- *      predictionsForMultiStops, schedule, messages,
- *      vehicleLocations, 
- */
-module.exports = function(command) {
+////////// code to get xml from url
+module.export = function(command) {
   var options = {
-    host: 'webservices.nextbus.com',
-    path: '/service/publicXMLFeed?a=stl&command=' + command
+    host: 'webservices.nextbus.com,
+    port: 80,
+    path: '/service/publicXMLFeed?a=stl&command='+command,
+    method: 'GET'
   };
-  
-  //function getResponse() {
-    /*http.get(options).on('response', function(res) {
-      var body = '';
-      res.on('data', function(chunk) {
-        body += chunk;
-      });
-      return res.on('end', function() {
-        console.log('parsing...');
-        var ret = libxmljs.parseXmlString(body);
-        console.log('parsed: ' + ret);
-        return ret;
-      });
+  var req = http.request(options, function(res) {
+    console.log('STATUS: ' + res.statusCode);
+    console.log('HEADERS: ' + JSON.stringify(res.headers));
+    res.setEncoding('utf8');
+    
+    var fullResponse = "";
+    
+    res.on('data', function (chunk) {
+      //console.log('BODY: ' + chunk);
+      fullResponse = fullResponse+chunk;
     });
-  }*/
+    
+    res.on('end', function(){
+      parser.parseString(fullResponse);
+      console.log(item.title + '\n');
+    });
+  });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
   
+  //code to parse xml
+  var jsonstring = "";
+  var jsonObj;
   var parser = new xml2js.Parser();
-  return fs.readFile( 'http://'+options.host+options.path, function(err, data) {
-      return parser.parseString(data, function (err, result) {
-          return console.dir(result);
-      });
+  parser.addListener('end', function(result) {
+      jsonstring = JSON.stringify(result);
+      jsonObj = JSON.parse(jsonstring);
+      
+      return jsonObj;
   });
 }
