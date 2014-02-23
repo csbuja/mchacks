4975f71466a6b5e0abe0cb1f74c69623003ceed4
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 20:02:08 -0500

    modified for require() function compliance

--- models/APIMaster.js
+++ models/APIMaster.js
@@ -8,17 +8,16 @@ var XmlStream = require('xml-stream');
  *      predictionsForMultiStops, schedule, messages,
  *      vehicleLocations, 
  */
-function getAPIData(command) {
+module.exports = function(options) {
+  var app = options.app; //not actually used
+  var command = options.param2;
   var options = {
     host: 'webservices.nextbus.com',
     path: '/service/publicXMLFeed?a=stl&command=' + command;
   };
   
   return http.get(options).on('response', function(response) {
-  
     var xml = new XmlStream(response, 'utf8');
-  
     return libxmljs.parse(xml);
-    
   });
 }
