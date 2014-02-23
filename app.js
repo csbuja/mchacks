689f0baa522728c65b13b3e3f445cfd71f9b308d
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 20:30:14 -0500

    added handler for get request on APItest

--- app.js
+++ app.js
@@ -99,6 +99,7 @@ app.use(express.errorHandler());
  */
 
 app.get('/', homeController.index);
+app.get('/apitest', apiTestController.index);
 app.get('/login', userController.getLogin);
 app.post('/login', userController.postLogin);
 app.get('/logout', userController.logout);
