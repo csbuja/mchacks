6a9cd3b3f6537acb30df36908a6b29292445c639
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 03:09:38 -0500

    Update APIHandler.js

--- models/APIHandler.js
+++ models/APIHandler.js
@@ -4,41 +4,39 @@ var http = require('http'),
 
 ////////// code to get xml from url
 exports.model = function(command){
-  function getData(command) {
-    var options = {
-      host: 'webservices.nextbus.com',
-      port: 80,
-      path: '/service/publicXMLFeed?a=stl&command='+command,
-      method: 'GET'
-    };
-    var req = http.request(options, function(res) {
-      res.setEncoding('utf8');
-      
-      var fullResponse = "";
-      
-      res.on('data', function (chunk) {
-        fullResponse += chunk;
-        console.log('chunk logged: ' + chunk);
-      });
-      
-      return res.on('end', function(){
-        console.log(fullResponce);
-        return parser.parseString(fullResponse);
-      });
-    });
-    req.on('error', function(e) {
-      console.log('problem with request: ' + e.message);
+  var options = {
+    host: 'webservices.nextbus.com',
+    port: 80,
+    path: '/service/publicXMLFeed?a=stl&command='+command,
+    method: 'GET'
+  };
+  var req = http.request(options, function(res) {
+    res.setEncoding('utf8');
+    
+    var fullResponse = "";
+    
+    res.on('data', function (chunk) {
+      fullResponse += chunk;
+      console.log('chunk logged: ' + chunk);
     });
     
-    //code to parse xml
-    var jsonstring = "";
-    var jsonObj;
-    var parser = new xml2js.Parser();
-    parser.addListener('end', function(result) {
-        jsonstring = JSON.stringify(result);
-        jsonObj = JSON.parse(jsonstring);
-        
-        return jsonObj;
+    return res.on('end', function(){
+      console.log(fullResponce);
+      return parser.parseString(fullResponse);
     });
-  }
+  });
+  req.on('error', function(e) {
+    console.log('problem with request: ' + e.message);
+  });
+  
+  //code to parse xml
+  var jsonstring = "";
+  var jsonObj;
+  var parser = new xml2js.Parser();
+  parser.addListener('end', function(result) {
+      jsonstring = JSON.stringify(result);
+      jsonObj = JSON.parse(jsonstring);
+      
+      return jsonObj;
+  });
 }
