bbd20a6fd26799029ddfadd95132ed2e5853340f
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 19:52:59 -0500

    Updated as function returning js obj from command

--- models/APIMaster.js
+++ models/APIMaster.js
@@ -1,3 +1,24 @@
 var libxmljs = require('xmljs');
+var http = require('http');
+var XmlStream = require('xml-stream');
 
-masterSource = undefined;
+/* return data to caller
+ * param: (string) commandType
+ *  valid values: routeList, routeConfig, predictions,
+ *      predictionsForMultiStops, schedule, messages,
+ *      vehicleLocations, 
+ */
+function getAPIData(command) {
+  var options = {
+    host: 'webservices.nextbus.com',
+    path: '/service/publicXMLFeed?a=stl&command=' + command;
+  };
+  
+  return http.get(options).on('response', function(response) {
+  
+    var xml = new XmlStream(response, 'utf8');
+  
+    return libxmljs.parse(xml);
+    
+  });
+}
