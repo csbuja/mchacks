5fcf95e32de8372c639ecc7d7ab82fcd4ebfbe48
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 09:42:29 -0500

    changed enter support again

--- public/js/results_handler.js
+++ public/js/results_handler.js
@@ -13,13 +13,13 @@ $(document).ready(function () {
 
 	initializeMap();
 
-	$('.header-form').submit( function(event){
+	$('#header-form').submit( function(event){
+		event.preventDefault();
 		console.log("submit button");
 		$('.result').remove();
 		directionsDisplay.setMap(null);
 		clearOverlays();
 		geocodeAddress();
-		event.preventDefault();
 	});
 
 	$('#clear').click( function(){ 
