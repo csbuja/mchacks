4df9a3d25edf85eb72c8ca6545fb8be59a4f1c55
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 03:39:00 -0500

    Update APIHandler.js

--- models/APIHandler.js
+++ models/APIHandler.js
@@ -3,38 +3,40 @@ var http = require('http'),
   xml2js = require('xml2js');
 
 ////////// code to get xml from url
-module.exports = function(command) {
-  console.log('export...'+command);
-  var options = {
-    host: 'webservices.nextbus.com',
-    port: 80,
-    path: '/service/publicXMLFeed?a=stl&command='+command,
-    method: 'GET'
-  };
-  return http.request(options, function(res) {
-    res.setEncoding('utf8');
-    
-    var fullResponse = "";
-    
-    res.on('data', function (chunk) {
-      fullResponse += chunk;
-      console.log('chunk logged: ' + chunk);
-    });
-    
-    return res.on('end', function(){
-      console.log(fullResponce);
-      return parser.parseString(fullResponse);
-    });
-  });
-  
-  //code to parse xml
-  var jsonstring = "";
-  var jsonObj;
-  var parser = new xml2js.Parser();
-  parser.addListener('end', function(result) {
-      jsonstring = JSON.stringify(result);
-      jsonObj = JSON.parse(jsonstring);
+module.exports = {
+  get: function(command) {
+    console.log('export...'+command);
+    var options = {
+      host: 'webservices.nextbus.com',
+      port: 80,
+      path: '/service/publicXMLFeed?a=stl&command='+command,
+      method: 'GET'
+    };
+    return http.request(options, function(res) {
+      res.setEncoding('utf8');
       
-      return jsonObj;
-  });
+      var fullResponse = "";
+      
+      res.on('data', function (chunk) {
+        fullResponse += chunk;
+        console.log('chunk logged: ' + chunk);
+      });
+      
+      return res.on('end', function(){
+        console.log(fullResponce);
+        return parser.parseString(fullResponse);
+      });
+    });
+    
+    //code to parse xml
+    var jsonstring = "";
+    var jsonObj;
+    var parser = new xml2js.Parser();
+    parser.addListener('end', function(result) {
+        jsonstring = JSON.stringify(result);
+        jsonObj = JSON.parse(jsonstring);
+        
+        return jsonObj;
+    });
+  }
 }
