9f17fec5dbd087c864c1ba442035c27612ae3eec
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 22:11:53 -0500

    Update RouteList.js

--- models/RouteList.js
+++ models/RouteList.js
@@ -2,5 +2,7 @@ var apiData = require('../models/APIHandler')('routeList');
 
 module.export = function(options) {
   //This needs more actual functionality
+  console.log(options);
+  console.log(apiData);
   return apiData;
 }
