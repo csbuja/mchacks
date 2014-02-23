cf40f5e28ce7262d7be6e9eb8028c417df7f32aa
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 19:35:47 -0500

    function now returns map, commented initializer

--- public/js/map.js
+++ public/js/map.js
@@ -1,12 +1,11 @@
-var map;
-function initialize() {
+function initializeMap() {
   var mapOptions = {
     disableDefaultUI: true,
     zoom: 8,
     center: new google.maps.LatLng(45.5833, -73.7500)
   };
-  map = new google.maps.Map(document.getElementById('map-canvas'),
+  return new google.maps.Map(document.getElementById('map-canvas'),
       mapOptions);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+//google.maps.event.addDomListener(window, 'load', initialize);
