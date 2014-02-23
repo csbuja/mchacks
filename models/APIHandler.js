74685f0118cda7cd85d758934a81ac76a7f9755e
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 03:26:32 -0500

    Update APIHandler.js

--- models/APIHandler.js
+++ models/APIHandler.js
@@ -3,7 +3,7 @@ var http = require('http'),
   xml2js = require('xml2js');
 
 ////////// code to get xml from url
-exports.module = function(command) {
+module.exports = function(command) {
   var options = {
     host: 'webservices.nextbus.com',
     port: 80,
