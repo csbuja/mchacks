6f0a2c9dae43b85b0c0e0408de90fe607e527025
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 04:22:31 -0500

    Update APIHandler.js

--- models/APIHandler.js
+++ models/APIHandler.js
@@ -7,6 +7,7 @@ module.exports = {
   get: function(command) {
     console.log('export...'+command);
     var parser = new xml2js.Parser();
+    var extractedData = "";
     var options = {
       host: 'webservices.nextbus.com',
       port: 80,
@@ -22,14 +23,11 @@ module.exports = {
         fullResponse += chunk;
       });
       
-      var extractedData = "";
       return parser.parseString(fullResponse, function(err,result){
         //Extract the value from the data element
         extractedData = result['data'];
-        console.log(extractedData);
       });
     });
-    return req.body;
-    return {};
+    return extractedData;
   }
 }
