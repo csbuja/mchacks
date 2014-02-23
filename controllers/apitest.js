958dd7106ffd73ec04962d025876f2f946cb7ead
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 04:51:35 -0500

    Update apitest.js

--- controllers/apitest.js
+++ controllers/apitest.js
@@ -2,20 +2,20 @@
 * GET /
 * Home page.
 */
-var xml2js = require('xml2js'),
-  http = require('http');
+var fs = require('fs'),
+    eyes = require('eyes'),
+    xml2js = require('xml2js');
 
 exports.index = function(req, res) {
   //get the data from the api
+   
   var parser = new xml2js.Parser();
-  var extractedData = "";
-  var fullResponse = "";
-  var options = {
-    host: 'webservices.nextbus.com',
-    port: 80,
-    path: '/service/publicXMLFeed?a=stl&command=routeList',
-    method: 'GET'
-  };
+  
+  parser.on('end', function(result) {
+    eyes.inspect(result);
+  });
+  
+  //get xml
   var request = http.request(options, function(response) {
     response.setEncoding('utf8');
     
@@ -24,15 +24,11 @@ exports.index = function(req, res) {
       console.log(chunk);
     });
 
-    parser.parseString(fullResponse, function(err,result){
-      //Extract the value from the data element
-      extractedData = result['data'];
-    });
+    parser.parseString(fullResponse);
   });
-  console.log(fullResponse);
-  console.log(extractedData);
+  
   res.render('apitest', {
     title: 'API Test',
-    data: extractedData
+    data: {}
   });
 };
