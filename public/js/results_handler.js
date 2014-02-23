14129e757632378fa155a8e9044c57d4604a867b
Jackson <jessupjn@umich.edu>
2014-02-22 20:10:09 -0500

    testing bus routes and drop pins

--- public/js/results_handler.js
+++ public/js/results_handler.js
@@ -1,10 +1,12 @@
 var map; // map object
+var directionsDisplay;
+var directionsService;
 var markers = []; // markers that are on the map.
 var clicked = "";
 
 $(document).ready(function () {
 
-	map = initializeMap();
+	initializeMap();
 
 	// FROM should automatically be set to current location (or a location chosen by user)
 	// DESTINATION triggers a search and loads results into the results_pg.
@@ -26,8 +28,7 @@ $(document).ready(function () {
 		console.log('click click');
 		clicked = $(this).id;
 
-		dropPin(45.5, 73.7);
-		dropPin(45.2, 74);
+		dropPin(45.5, 73.7, 45.2, 74);
 
 	});
 
@@ -36,29 +37,51 @@ $(document).ready(function () {
 function performSearch(){
 	//String s = $('.results_pg').children().length.toString();
 	// $('.results_pg').append( $('.result #'+s.toString() ) );
-
 };
 
 
 
 // builds the map object
 function initializeMap() {
-  var mapOptions = {
-    disableDefaultUI: true,
-    zoom: 8,
-    center: new google.maps.LatLng(45.5833, -73.7500)
-  };
-  return new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
 
-function dropPin(lat, lng){
+	var mapOptions = {
+    	disableDefaultUI: true,
+    	zoom: 8,
+		center: new google.maps.LatLng(45.5833, -73.7500)
+	};
+  	map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+
+	// sets up the directions to display on the map
+	directionsService = new google.maps.DirectionsService();
+	directionsDisplay = new google.maps.DirectionsRenderer();
+	directionsDisplay.setMap(map);
+};
+
+function dropPin(lat, lng, lat2, lng2){
 	var position = new google.maps.LatLng( lat, lng);
+	var position2 = new google.maps.LatLng( lat2, lng2);
 
 	markers.push( new google.maps.Marker({
-				position: pos,
+				position: position,
 				map: this.map,
 				draggable: false,
 				animation: google.maps.Animation.DROP
 			}));
+	markers.push( new google.maps.Marker({
+				position: position2,
+				map: this.map,
+				draggable: false,
+				animation: google.maps.Animation.DROP
+			}));
+	var request = {
+      origin:position,
+      destination:position2,
+      travelMode: google.maps.TravelMode.TRANSIT
+  	};
+  	directionsService.route(request, function(response, status) {
+    	if (status == google.maps.DirectionsStatus.OK) {
+      		directionsDisplay.setDirections(response);
+    	}
+  });
 }
