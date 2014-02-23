eff5ba6cc53a196af3f82a3606bdbb437a8a14b2
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 20:22:34 -0500

    fixed app.js which had been broken

--- app.js
+++ app.js
@@ -16,7 +16,6 @@ var connectAssets = require('connect-assets');
  */
 
 var homeController = require('./controllers/home');
-var apiTestController = require('./controllers/user');
 var userController = require('./controllers/user');
 var apiController = require('./controllers/api');
 var contactController = require('./controllers/contact');
@@ -110,7 +109,6 @@ app.get('/signup', userController.getSignup);
 app.post('/signup', userController.postSignup);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
-app.get('/apitest', apiTestController.index); //TODO this is temporary
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
