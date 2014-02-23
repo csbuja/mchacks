c0c5e5f604648927fde89673b537940cc80afe5c
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-22 20:38:18 -0500

    commented require/data send

--- controllers/apitest.js
+++ controllers/apitest.js
@@ -3,11 +3,11 @@
 * Home page.
 */
 
-var data = require('/models/RouteList.js');
+//var data = require('/models/RouteList.js');
 
 exports.index = function(req, res) {
   res.render('apitest', {
-    title: 'API Test',
-    data: data
+    title: 'API Test'/*,
+    data: data*/
   });
 };
