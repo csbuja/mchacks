0ff5dda83f44126ec0cac380f4413cd045b0b92c
Nathan Mealey <nathan.r.mealey@gmail.com>
2014-02-23 02:45:55 -0500

    Update apitest.js

--- controllers/apitest.js
+++ controllers/apitest.js
@@ -8,7 +8,8 @@ var routeList = require('../models/RouteList');
 exports.index = function(req, res) {
   res.render('apitest', {
     title: 'API Test',
-    data: routeList
+    data: routeList.getData()
   });
-  console.log(routeList);
+  console.log('route list');
+  console.log(routeList.getData());
 };
